6bd021d26a29df3ec8586a3ada3baf77116cc8bd
Mergen Nuryyev <m.nuryyev@iqfulfillment.com>
2023-07-02 00:05:48 +0400

    added validation for the notes

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-//dd(parse_url($_SERVER['REQUEST_URI'])['path']);
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require 'routes.php';
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
@@ -27,4 +19,5 @@ function abort($code = 404) {
     die();
 }
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 routeToController($uri, $routes);
