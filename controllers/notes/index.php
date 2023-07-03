4834b8d61b96819200502fce9af0d48724885d1b
Mergen Nuryyev <m.nuryyev@iqfulfillment.com>
2023-07-03 23:14:52 +0400

    build route and one request one controller

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,10 +1,12 @@
 <?php
 
-$config = require 'config.php';
+use Core\Database;
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-
-$heading = "My Notes";
 $notes = $db->query('SELECT * FROM notes where user_id = 1')->get();
-//dd($notes);
-require "views/notes/index.view.php";
+
+view("notes/index.view.php", [
+    'heading' => 'My Notes',
+    'notes' => $notes
+]);
